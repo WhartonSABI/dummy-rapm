4f14ca5272aa69665a66d248d57243c70ad9d9e0
kennyws <landowatts@gmail.com>
2026-04-30 12:53:48 -0400

    yur

--- Weighted Stint RAPM.R
+++ Weighted Stint RAPM.R
@@ -251,7 +251,7 @@ play_by_play_data_small <- play_by_play_data_small %>%
 # Literature has had filter at ~200 minutes played
 
 # ~200 possessions in a game
-p <- 1000
+p <- 800
 
 qualified_players <- play_by_play_data_small %>%
   group_by(game_id, possession_id) %>%
